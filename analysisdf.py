c291a170fd3ed967952532c3ee6a376cd78165e5
Ioanna Papagianni <ioannapap@users.noreply.github.com>
2019-11-10 03:25:23 +0200

    Add files via upload

--- analysisdf.py
+++ analysisdf.py
@@ -9,6 +9,7 @@ from scipy import stats
 
 df = pd.read_csv('movies.csv')
 
+
 #removing unnecessary columns
 df.drop(['Unnamed: 0', 'Unnamed: 0.1', 'US Gross', 'US DVD Sales', 'MPAA Rating', 'Running Time (min)', 'Distributor', 'Source', 'Creative Type'], axis = 1, inplace = True)
 
@@ -22,5 +23,11 @@ df = df[df.Gross != '0']
 #fixing indices after pruning (starting from 1 instead of 0)
 df.index = np.arange(1, len(df) + 1)
 
+#converting Gross from str to int
+df.Gross = df['Gross'].astype(int)
+
 #fixing the format of Date
-df.Date =  df['Date'].str[-2:]
+df.Date = df['Date'].str[-2:]
+df.replace('BD', np.NaN, inplace = True)
+
+print(df)
